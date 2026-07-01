1c0186fd9d9c47ee364f81053dcf6cbf4b27b7a3
Haythem <haythemdridi633@gmail.com>
2026-07-01 14:09:06 +0100

    sort

--- backend/internal/services/product_service.go
+++ backend/internal/services/product_service.go
@@ -1,10 +1,34 @@
 package services
 
 import (
+	"sort"
+	"strconv"
+	"strings"
 	"tunisianet-scraper/internal/models"
 	"tunisianet-scraper/internal/scraper"
 )
 
+func extractPrice(price string) float64 {
+	price = strings.TrimSpace(price)
+
+	price = strings.ReplaceAll(price, "DT", "")
+	price = strings.ReplaceAll(price, "TND", "")
+
+	// remove every type of space
+	price = strings.ReplaceAll(price, " ", "")
+	price = strings.ReplaceAll(price, "\u00A0", "")
+	price = strings.ReplaceAll(price, "\u202F", "")
+
+	price = strings.ReplaceAll(price, ",", ".")
+
+	value, err := strconv.ParseFloat(price, 64)
+	if err != nil {
+		println("Parse error:", price)
+		return 0
+	}
+
+	return value
+}
 func SearchProducts(search string) []models.Product {
 
 	var products []models.Product
@@ -13,6 +37,31 @@ func SearchProducts(search string) []models.Product {
 	products = append(products, scraper.SearchMyTek(search)...)
 	products = append(products, scraper.SearchWiki(search)...)
 	products = append(products, scraper.SearchSpaceNet(search)...)
+	for _, p := range products {
+		println(
+			p.Store,
+			"|",
+			p.Price,
+			"|",
+			strconv.FormatFloat(extractPrice(p.Price), 'f', 3, 64),
+		)
+	}
+
+	sort.Slice(products, func(i, j int) bool {
+		return extractPrice(products[i].Price) < extractPrice(products[j].Price)
+	})
+
+	println("========== AFTER SORT ==========")
+
+	for i := 0; i < 20 && i < len(products); i++ {
+		println(
+			products[i].Store,
+			"|",
+			products[i].Price,
+			"|",
+			strconv.FormatFloat(extractPrice(products[i].Price), 'f', 3, 64),
+		)
+	}
 
 	return products
 }
